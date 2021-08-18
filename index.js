71fab343ad018fb09689c96a9d63921c694f83d1
CuJo33 <77585826+CuJo33@users.noreply.github.com>
2021-08-18 16:02:26 +0100

    added carousel

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("music").style.display = "none";
+
+function load1() {
+  window.open("https://cujo33.github.io/OOPGame/");
+}
+
+function load2() {
+  window.open("https://cujo33.github.io/Quiz/");
+}
+
+function load3() {
+  window.open("https://cujo33.github.io/HobbyWebsite/");
+}
